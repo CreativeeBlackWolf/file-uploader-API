5dcd06758ff15882c68c2046ca43cf09236ff636
CreativeeBlackWolf <32525436+CreativeeBlackWolf@users.noreply.github.com>
2024-02-03 20:25:50 +0300

    uncomment try-except

--- app/api/views.py
+++ app/api/views.py
@@ -20,18 +20,20 @@ class UploadFileAPIView(APIView):
 
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
-        # try:
-        with transaction.atomic():
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
-                return JsonResponse(serializer.instance, status=status.HTTP_201_CREATED)
-            else:
-                return JsonResponse(
-                    {"error": "file was not uploaded"},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-        # except Exception as e:
-        #     print(str(e))
-        #     return JsonResponse(
-        #         {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-        #     )
+        try:
+            with transaction.atomic():
+                if serializer.is_valid(raise_exception=True):
+                    serializer.save()
+                    return JsonResponse(
+                        serializer.instance, status=status.HTTP_201_CREATED
+                    )
+                else:
+                    return JsonResponse(
+                        {"error": "file was not uploaded"},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+        except Exception as e:
+            print(str(e))
+            return JsonResponse(
+                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
