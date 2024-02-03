957ce124b474325e692b016d8900812b5a9b8cab
creativeeblackwolf <creativeeblackwolf>
2024-02-03 16:28:45 +0300

    autoretry if field doesn't exist

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -3,10 +3,14 @@ from celery import shared_task
 from api.models import File
 import os
 import time
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import connection
 
 
-@shared_task
+@shared_task(
+    autoretry_for=(ObjectDoesNotExist,),
+    retry_kwargs={"max_retries": 10, "countdown": 0.5},
+)
 def handle_file(file_id: int) -> bool:
     time.sleep(0.1)  # slow down to make sure that DB was updated
     file = File.objects.get(pk=file_id)
@@ -16,7 +20,7 @@ def handle_file(file_id: int) -> bool:
     file.data = {
         "filename": filename,
         "extension": file_extension,
-        "size": f"{os.path.getsize(filepath) / (1024 ** 2)}",
+        "size": f"{round(os.path.getsize(filepath) / (1024 ** 2), 2)}",
     }
 
     file.processed = True
